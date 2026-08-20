dc52163ade49a12ac3b72fdfb9312bb298be0edd
ishan <ishansinha2008@gmail.com>
2026-08-20 22:44:01 +0530

    Fix: Add createClub route to user routes for club creation functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { registerEvent } from "../controllers/user.contollers.js"
+import { createClub } from "../controllers/user.contollers.js"
 
 const router = Router()
 
 router.route("/registerEvent").post(registerEvent)
+router.route("/createClub").post(createClub)
 
 export default router
