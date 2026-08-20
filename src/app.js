8ca3251cf9eacf6718944699d0165bb37052e9bd
ishan <ishansinha2008@gmail.com>
2026-08-20 21:45:51 +0530

    Fix: Uncomment userRouter import and route declaration in app.js

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ app.use(cookieParser())
 
 //routes import
 
-// import userRouter from './routes/user.routes.js'
+import userRouter from './routes/user.routes.js'
 
 //routes declaration
-// app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter)
 
 
 //https://localhost:8000/api/v1/users/registerUser
